ff86f0984f869eb14f0695a12335bf67bc977fdc
Anton <patsev.anton@gmail.com>
2025-11-03 10:43:07 +0600

    add sa-dns-manager

--- terraform/service-accounts.tf
+++ terraform/service-accounts.tf
@@ -8,3 +8,14 @@ resource "yandex_resourcemanager_folder_iam_member" "sa-k8s-admin-permissions" {
   role      = "admin"
   member    = "serviceAccount:${yandex_iam_service_account.sa-k8s-admin.id}"
 }
+
+resource "yandex_iam_service_account" "sa-dns-manager" {
+  folder_id = local.folder_id
+  name      = "sa-dns-manager"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "sa-dns-manager-permissions" {
+  folder_id = local.folder_id
+  role      = "dns.editor"
+  member    = "serviceAccount:${yandex_iam_service_account.sa-dns-manager.id}"
+}
