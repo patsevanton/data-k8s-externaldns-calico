bd6bbc13ad8179804260ada8bef8da1e729c59c5
Anton <patsev.anton@gmail.com>
2025-11-03 13:57:02 +0600

    disable cilium

--- terraform/cilium.tf
+++ terraform/cilium.tf
@@ -1,64 +1,64 @@
-resource "yandex_kubernetes_cluster" "cilium" {
-  name        = "cilium"
-  network_id  = yandex_vpc_network.k8s-network.id
+# resource "yandex_kubernetes_cluster" "cilium" {
+#   name        = "cilium"
+#   network_id  = yandex_vpc_network.k8s-network.id
 
-  master {
-    version = "1.31"
-    zonal {
-      zone      = yandex_vpc_subnet.k8s-subnet.zone
-      subnet_id = yandex_vpc_subnet.k8s-subnet.id
-    }
-    public_ip = true
-  }
+#   master {
+#     version = "1.31"
+#     zonal {
+#       zone      = yandex_vpc_subnet.k8s-subnet.zone
+#       subnet_id = yandex_vpc_subnet.k8s-subnet.id
+#     }
+#     public_ip = true
+#   }
 
-  service_account_id      = yandex_iam_service_account.sa-k8s-admin.id
-  node_service_account_id = yandex_iam_service_account.sa-k8s-admin.id
-  release_channel         = "STABLE"
-  network_policy_provider = "CALICO"
-  cluster_ipv4_range      = "10.113.0.0/16"
-  service_ipv4_range      = "10.97.0.0/16"
+#   service_account_id      = yandex_iam_service_account.sa-k8s-admin.id
+#   node_service_account_id = yandex_iam_service_account.sa-k8s-admin.id
+#   release_channel         = "STABLE"
+#   network_policy_provider = "CALICO"
+#   cluster_ipv4_range      = "10.113.0.0/16"
+#   service_ipv4_range      = "10.97.0.0/16"
 
-  depends_on = [yandex_resourcemanager_folder_iam_member.sa-k8s-admin-permissions]
-}
+#   depends_on = [yandex_resourcemanager_folder_iam_member.sa-k8s-admin-permissions]
+# }
 
-resource "yandex_kubernetes_node_group" "k8s_node_group_cilium" {
-  cluster_id  = yandex_kubernetes_cluster.cilium.id
-  name        = "node-group-cilium"
-  version     = "1.31"
+# resource "yandex_kubernetes_node_group" "k8s_node_group_cilium" {
+#   cluster_id  = yandex_kubernetes_cluster.cilium.id
+#   name        = "node-group-cilium"
+#   version     = "1.31"
 
-  instance_template {
-    platform_id = "standard-v3"
+#   instance_template {
+#     platform_id = "standard-v3"
 
-    network_interface {
-      nat        = true
-      subnet_ids = [yandex_vpc_subnet.k8s-subnet.id]
-    }
+#     network_interface {
+#       nat        = true
+#       subnet_ids = [yandex_vpc_subnet.k8s-subnet.id]
+#     }
 
-    resources {
-      cores  = 2
-      memory = 8
-    }
+#     resources {
+#       cores  = 2
+#       memory = 8
+#     }
 
-    boot_disk {
-      type = "network-ssd"
-      size = 65
-    }
+#     boot_disk {
+#       type = "network-ssd"
+#       size = 65
+#     }
 
-    metadata = {
-      ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
-    }
+#     metadata = {
+#       ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+#     }
 
-  }
+#   }
 
-  scale_policy {
-    fixed_scale {
-      size = 1
-    }
-  }
+#   scale_policy {
+#     fixed_scale {
+#       size = 1
+#     }
+#   }
 
-  allocation_policy {
-    location {
-      zone = "ru-central1-b"
-    }
-  }
-}
+#   allocation_policy {
+#     location {
+#       zone = "ru-central1-b"
+#     }
+#   }
+# }
