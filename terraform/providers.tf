713e240ec650f4bd012a7cb1c5f1f81561ac1eff
Anton <patsev.anton@gmail.com>
2025-11-30 09:27:17 +0600

    add required_providers helm

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,8 +2,12 @@ terraform {
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = "~> 0.102.0"
+      version = ">= 0.72.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.0.0, < 3.0.0"
     }
   }
-  required_version = ">= 1.0"
+  required_version = ">= 1.3"
 }
