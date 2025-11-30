1f7aa26b216bdcbe8071db03d70428c27481d0e5
Anton <patsev.anton@gmail.com>
2025-11-30 11:52:21 +0600

    install contour

--- terraform/cilium-redis.tf
+++ terraform/cilium-redis.tf
@@ -77,20 +77,19 @@ provider "helm" {
   }
 }
 
-# Установка ingress-nginx через Helm
-resource "helm_release" "ingress_nginx" {
-  name             = "ingress-nginx"
-  repository       = "https://kubernetes.github.io/ingress-nginx"
-  chart            = "ingress-nginx"
-  version          = "4.10.6"
-  namespace        = "ingress-nginx"
+# Установка Contour через Helm (соответствует командам helm repo add contour https://projectcontour.github.io/helm-charts/, helm repo update, helm install contour contour/contour ...)
+resource "helm_release" "contour" {
+  name             = "contour"
+  repository       = "https://projectcontour.github.io/helm-charts/"
+  chart            = "contour"
+  namespace        = "contour"
   create_namespace = true
   depends_on       = [yandex_kubernetes_node_group.k8s_node_group_cilium_redis]
 
   set = [
     {
-      name  = "controller.service.loadBalancerIP"
-      value = yandex_vpc_address.addr.external_ipv4_address[0].address  # Присвоение внешнего IP ingress-контроллеру
+      name  = "contour.service.loadBalancerIP"
+      value = yandex_vpc_address.addr.external_ipv4_address[0].address  # Присвоение внешнего IP балансировщику
     }
   ]
 }
