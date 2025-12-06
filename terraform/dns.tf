adc423241e336715f92c2bd7df278a26a9218905
Anton <patsev.anton@gmail.com>
2025-12-06 09:09:43 +0600

    apatsev_corp_zone

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,19 +1,27 @@
 # Создание внешнего IP-адреса в Yandex Cloud
-resource "yandex_vpc_address" "addr" {
-  name = "cilium-redis"  # Имя ресурса внешнего IP-адреса
+resource "yandex_vpc_address" "addr_redis" {
+  name = "redis"  # Имя ресурса внешнего IP-адреса
 
   external_ipv4_address {
     zone_id = yandex_vpc_subnet.k8s-subnet.zone  # Зона доступности, где будет выделен IP-адрес
   }
 }
 
-resource "yandex_dns_zone" "data_k8s_zone" {
-  name             = "data-k8s-zone"
-  zone             = "data.k8s.mycompany.corp."
-  public           = false
+resource "yandex_dns_zone" "apatsev_corp_zone" {
+  name   = "apatsev-corp"
+  zone   = "apatsev.corp."
+  public = false
   private_networks = [yandex_vpc_network.k8s-network.id]
 }
 
+resource "yandex_dns_recordset" "redis_apatsev_corp" {
+  zone_id = yandex_dns_zone.apatsev_corp_zone.id
+  name    = "redis.apatsev.corp."
+  type    = "A"
+  ttl     = 200
+  data    = [yandex_vpc_address.addr_redis.external_ipv4_address[0].address]
+}
+
 output "dns_zone_id" {
-  value = yandex_dns_zone.data_k8s_zone.id
+  value = yandex_dns_zone.apatsev_corp_zone.id
 }
